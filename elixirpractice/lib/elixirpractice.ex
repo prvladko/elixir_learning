8809066143bd56098fb589f13287ba8201c401c7
prvladko <prvladko@gmail.com>
2022-11-01 17:16:05 +0200

    guessing game

---
+++ elixirpractice/lib/elixirpractice.ex
@@ -0,0 +1,18 @@
+defmodule Elixirpractice do
+  @moduledoc """
+  Documentation for `Elixirpractice`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirpractice.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
